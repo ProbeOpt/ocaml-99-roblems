d31262862a0294c86e22b07e1b87a1fd8df6aff9
ProbeOpt <a@b.c>
2026-06-26 15:53:46 +0500

    control_flow.ml: fizzbuzz2

--- learn_ocaml/control_flow.ml
+++ learn_ocaml/control_flow.ml
@@ -7,5 +7,18 @@ let fizzbuzz n =
     | _ -> Printf.printf "%d\n" x
   done
 
+let fizzbuzz2 n =
+   for x = 1 to n do
+     if x mod 15 == 0 then begin
+       Printf.printf "FizzBuzz\n"
+     end else if x mod 3 == 0 then begin
+       Printf.printf "Fizz\n"
+     end else if x mod 5 == 0 then begin
+       Printf.printf "Buzz\n"
+     end else begin
+       Printf.printf "%d\n" x
+     end
+  done
+
 let () =
-  fizzbuzz 100
+   fizzbuzz2 100;
